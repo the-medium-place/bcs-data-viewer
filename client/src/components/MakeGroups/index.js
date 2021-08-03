facecb13ed68ab940d30de6431126d0effe70121
Zac <zgstowell@gmail.com>
2021-08-03 11:31:27 -0700

    styling forms etc

--- client/src/components/MakeGroups/index.js
+++ client/src/components/MakeGroups/index.js
@@ -1,239 +1,303 @@
-import React, { useState, useEffect } from 'react'
-import API from '../../utils/API';
-import { useMutation } from '@apollo/client';
-import { SAVE_GROUPS } from '../../utils/mutations'
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
+import { useMutation } from "@apollo/client";
+import { SAVE_GROUPS } from "../../utils/mutations";
 
+export default function MakeGroups({
+  loggedInUser,
+  studentRoster,
+  droppedStudents,
+  bcsCohortId,
+  enrollmentId,
+  cohortId,
+  cohortGroups,
+}) {
+  const bcsEmail = loggedInUser.bcsLoginInfo.bcsEmail;
+  const bcsPassword = loggedInUser.bcsLoginInfo.bcsPassword;
+  const activeStudents = studentRoster.filter(
+    (student) => !droppedStudents.includes(student)
+  );
+  const MAP_GRADES_TO_INT = {
+    "A+": 1,
+    A: 2,
+    "A-": 3,
+    "B+": 4,
+    B: 5,
+    "B-": 6,
+    "C+": 7,
+    C: 8,
+    "C-": 9,
+    "D+": 10,
+    D: 11,
+    "D-": 12,
+    F: 13,
+    I: 13,
+    "Overdue!": 13,
+    "Not Due!": 13,
+    Ungraded: 13,
+    "N/A": 0,
+  };
 
-export default function MakeGroups({ loggedInUser, studentRoster, droppedStudents, bcsCohortId, enrollmentId, cohortId, cohortGroups }) {
+  const [saveGroups, { error, data }] = useMutation(SAVE_GROUPS);
+  if (error) {
+    console.log(JSON.stringify(error));
+  }
 
+  const [gradeData, setGradeData] = useState(null);
+  const [numGroups, setNumGroups] = useState(6);
+  const [gradesArr, setGradesArr] = useState([]);
+  const [groups, setGroups] = useState(null);
+  const [showGroups, setShowGroups] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const [disableSave, setDisableSave] = useState(false);
+  const [memberRepeat, setMemberRepeat] = useState(true);
 
-    const bcsEmail = loggedInUser.bcsLoginInfo.bcsEmail;
-    const bcsPassword = loggedInUser.bcsLoginInfo.bcsPassword;
-    const activeStudents = studentRoster.filter(student => !droppedStudents.includes(student))
-    const MAP_GRADES_TO_INT = {
-        'A+': 1,
-        'A': 2,
-        'A-': 3,
-        'B+': 4,
-        'B': 5,
-        'B-': 6,
-        'C+': 7,
-        'C': 8,
-        'C-': 9,
-        'D+': 10,
-        'D': 11,
-        'D-': 12,
-        'F': 13,
-        'I': 13,
-        'Overdue!': 13,
-        'Not Due!': 13,
-        'Ungraded': 13,
-        'N/A': 0
+  async function fetchData() {
+    setGradeData(
+      await API.getGradeData(bcsEmail, bcsPassword, bcsCohortId, enrollmentId)
+    );
+  }
+  // GET AND STORE ALL GRADE DATA
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // GET AVERAGE GRADE FOR SINGLE STUDENT
+  const getGradeAvg = (student) => {
+    let numDue = 0,
+      totalGradeVal = 0,
+      gradeAvg = 0;
+
+    gradeData.studentObj[student].assignments.forEach((assignmentObj) => {
+      if (assignmentObj.isDue) {
+        numDue++;
+        totalGradeVal += MAP_GRADES_TO_INT[assignmentObj.grade];
+      }
+    });
+    gradeAvg = totalGradeVal / (numDue || 1);
+    return { student, gradeAvg };
+  };
+
+  const handleGroupNumChange = (e) => {
+    const value = e.target.value;
+    // SHOW THE 'MAKE GROUPS' BUTTON
+    setShowButton(true);
+
+    // RESET gradesArr STATE
+    setGradesArr([]);
+
+    // UPDATE numGroups STATE
+    if (value <= 0) {
+      setNumGroups(1);
+    }
+    setNumGroups(value);
+  };
+
+  /**
+   * Groups student objects into chunks
+   * @returns groupsObj
+   */
+  const chunkArray = () => {
+    // EMPTY OBJECT TO HOLD FINAL GROUPS
+    const groupsObj = {};
+
+    // SORT STUDENT ARRAY BY AVERAGE GRADE VALUE
+    const sortedArr = gradesArr.sort((a, b) => a.gradeAvg - b.gradeAvg);
+    // console.log(sortedArr)
+
+    for (let i = 1; i <= numGroups; i++) {
+      // CREATE KEY IN groupsObj FOR EACH GROUP
+      groupsObj[`Group ${i}`] = [];
     }
 
-    const [saveGroups, { error, data }] = useMutation(SAVE_GROUPS);
-    if (error) { console.log(JSON.stringify(error)) }
+    // PUSH STUDENTS INTO GROUPS ONE-BY-ONE IN ORDER --
+    // SINCE STUDENTS ARE SORTED BY GRADE, DIFFERENT GRADE
+    // AVERAGES WILL BE SPREAD EVENLY THROUGHOUT THE GROUPS
+    let counter = 1;
+    sortedArr.forEach((gradeObj) => {
+      if (counter <= numGroups) {
+        groupsObj[`Group ${counter}`].push(gradeObj);
+        counter++;
+      } else {
+        counter = 1;
+        groupsObj[`Group ${counter}`].push(gradeObj);
+        counter++;
+      }
+    });
 
-    const [gradeData, setGradeData] = useState(null);
-    const [numGroups, setNumGroups] = useState(6);
-    const [gradesArr, setGradesArr] = useState([])
-    const [groups, setGroups] = useState(null);
-    const [showGroups, setShowGroups] = useState(false);
-    const [showButton, setShowButton] = useState(true);
-    const [disableSave, setDisableSave] = useState(false);
-    const [memberRepeat, setMemberRepeat] = useState(true);
+    // console.log(groupsObj)
+    return groupsObj;
+  };
 
-    async function fetchData() {
-        setGradeData(await API.getGradeData(bcsEmail, bcsPassword, bcsCohortId, enrollmentId));
-    }
-    // GET AND STORE ALL GRADE DATA
-    useEffect(() => {
+  const handleGroupButtonClick = async (e) => {
+    e.preventDefault();
 
-        fetchData();
+    // HIDE THE 'MAKE GROUPS' BUTTON
+    setShowButton(false);
+    // ENABLE THE 'SAVE GROUPS' BUTTON
+    setDisableSave(false);
 
-    }, [])
+    // LOOP THROUGH ACTIVE STUDENTS
+    // getGradeAvg() RETURNS OBJECT --
+    // {student: <student name>, gradeAvg: <numerical representation of average grade>}
+    // PUSH RESULTING OBJECT TO gradesArr STATE
+    await activeStudents.forEach((student) => {
+      // console.log({ student })
+      const arrCopy = gradesArr;
+      arrCopy.push(getGradeAvg(student));
+      setGradesArr(arrCopy);
+    });
 
-    // GET AVERAGE GRADE FOR SINGLE STUDENT
-    const getGradeAvg = (student) => {
-        let numDue = 0,
-            totalGradeVal = 0,
-            gradeAvg = 0;
+    // UPDATE groups STATE
+    // chunkArray() RETURNS OBJECT --
+    // EACH KEY IS A GROUP AND VALUE IS AN ARRAY OF STUDENT OBJECTS MAKING UP THAT GROUP
+    const finalGroups = chunkArray();
+    console.log({ finalGroups });
+    await setGroups(finalGroups);
 
-        gradeData.studentObj[student].assignments.forEach(assignmentObj => {
-            if (assignmentObj.isDue) {
-                numDue++
-                totalGradeVal += MAP_GRADES_TO_INT[assignmentObj.grade]
-            }
-        })
-        gradeAvg = totalGradeVal / (numDue || 1);
-        return { student, gradeAvg };
-    }
+    // SHOW THE GROUPS
+    setShowGroups(true);
+  };
 
-    const handleGroupNumChange = e => {
-        const value = e.target.value;
-        // SHOW THE 'MAKE GROUPS' BUTTON
-        setShowButton(true);
+  const handleSaveClick = async (e) => {
+    e.preventDefault();
 
-        // RESET gradesArr STATE
-        setGradesArr([])
+    // DISABLE 'SAVE GROUPS' BUTTON
+    setDisableSave(true);
+    console.log({ groups, cohortId });
 
-        // UPDATE numGroups STATE
-        if (value <= 0) { setNumGroups(1) }
-        setNumGroups(value)
-    }
-
-    /**
-     * Groups student objects into chunks
-     * @returns groupsObj
-     */
-    const chunkArray = () => {
-        // EMPTY OBJECT TO HOLD FINAL GROUPS
-        const groupsObj = {};
-
-        // SORT STUDENT ARRAY BY AVERAGE GRADE VALUE
-        const sortedArr = gradesArr.sort((a, b) => a.gradeAvg - b.gradeAvg)
-        // console.log(sortedArr)
-
-        for (let i = 1; i <= numGroups; i++) {
-            // CREATE KEY IN groupsObj FOR EACH GROUP
-            groupsObj[`Group ${i}`] = [];
-        }
-
-        // PUSH STUDENTS INTO GROUPS ONE-BY-ONE IN ORDER --
-        // SINCE STUDENTS ARE SORTED BY GRADE, DIFFERENT GRADE
-        // AVERAGES WILL BE SPREAD EVENLY THROUGHOUT THE GROUPS
-        let counter = 1;
-        sortedArr.forEach(gradeObj => {
-            if (counter <= numGroups) {
-                groupsObj[`Group ${counter}`].push(gradeObj);
-                counter++;
-            } else {
-                counter = 1;
-                groupsObj[`Group ${counter}`].push(gradeObj);
-                counter++;
-            }
-        })
-
-
-        // console.log(groupsObj)
-        return groupsObj;
+    let groupTitle = prompt(
+      "Please enter a name for this selection of groups: "
+    );
+    // MAKE SURE USER ENTERS A TITLE
+    while (groupTitle.length === 0 || !groupTitle) {
+      groupTitle = prompt(
+        "You must enter a valid name for this selection of groups: "
+      );
     }
 
-    const handleGroupButtonClick = async (e) => {
-        e.preventDefault();
-
-        // HIDE THE 'MAKE GROUPS' BUTTON
-        setShowButton(false);
-        // ENABLE THE 'SAVE GROUPS' BUTTON
-        setDisableSave(false);
-
-        // LOOP THROUGH ACTIVE STUDENTS
-        // getGradeAvg() RETURNS OBJECT --
-        // {student: <student name>, gradeAvg: <numerical representation of average grade>}
-        // PUSH RESULTING OBJECT TO gradesArr STATE
-        await activeStudents.forEach(student => {
-            // console.log({ student })
-            const arrCopy = gradesArr;
-            arrCopy.push(getGradeAvg(student));
-            setGradesArr(arrCopy)
-        })
-
-        // UPDATE groups STATE
-        // chunkArray() RETURNS OBJECT --
-        // EACH KEY IS A GROUP AND VALUE IS AN ARRAY OF STUDENT OBJECTS MAKING UP THAT GROUP
-        const finalGroups = chunkArray()
-        console.log({ finalGroups })
-        await setGroups(finalGroups)
-
-        // SHOW THE GROUPS
-        setShowGroups(true);
+    try {
+      const { data } = await saveGroups({
+        variables: { title: groupTitle, groups, cohortId },
+      });
+      console.log(data);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const handleSaveClick = async e => {
-        e.preventDefault();
+  return (
+    <div className="MakeGroups mb-5">
+      <p className="text-center mt-3">
+        Use this tool to form groups for projects and/or class activities.
+        Groups are formed by calculating the 'average' grade for each student,
+        then evenly distributing students among the selected number of groups so
+        that each group has a mixture of performance levels.
+      </p>
 
-        // DISABLE 'SAVE GROUPS' BUTTON
-        setDisableSave(true);
-        console.log({ groups, cohortId })
-
-        let groupTitle = prompt('Please enter a name for this selection of groups: ');
-        // MAKE SURE USER ENTERS A TITLE
-        while (groupTitle.length === 0 || !groupTitle) {
-            groupTitle = prompt('You must enter a valid name for this selection of groups: ')
-        }
-
-        try {
-            const { data } = await saveGroups({
-                variables: { title: groupTitle, groups, cohortId },
-            })
-            console.log(data)
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
-
-    return (
-        <div className="MakeGroups mb-5">
-            <p className="text-center mt-3" >Use this tool to form groups for projects and/or class activities. Groups are formed by calculating the 'average' grade for each student, then evenly distributing students among the selected number of groups so that each group has a mixture of performance levels.</p>
-
-            {gradeData ? (
-
-                <div className="d-flex justify-content-center w-100 p-2 mt-2 mb-1 w-25 bg-dark text-light">
-                    <form onSubmit={handleGroupButtonClick} className="d-flex flex-column justify-content-center">
-                        <label className="form-label" htmlFor="numGroups"><strong>Desired number of groups:</strong></label>
-                        <input className="form-control mb-2" type="number" min="1" name="numGroups" id="numGroups" value={numGroups} onChange={handleGroupNumChange} />
-                        {cohortGroups.length > 0 ? (
-
-                            <div className="w-75 text-center">
-                                <label className="form-check-label" htmlFor="memberRepeat"><strong>Place students who have worked together before in separate groups (no repeating members):</strong></label>
-                                <input className="form--check-input" type="checkbox" checked={memberRepeat} onChange={() => setMemberRepeat(!memberRepeat)} />
-                            </div>
-                        ) : null}
-
-                        <div className="button-wrapper w-100 d-flex justify-content-center p-1" style={{ height: '3rem' }}>
-                            {showButton ? <button className="btn btn-secondary" type="submit" onClick={handleGroupButtonClick}>Form Groups</button> : null}
-                        </div>
-                    </form>
-                </div>
-
-            ) : (
-                <div className="d-flex align-items-center my-5 p-5">
-                    <strong>Preparing student data...</strong>
-                    <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-                </div>
-            )}
-            <div className="groups-wrapper d-flex justify-content-center flex-wrap" style={{ border: '1px solid black', }}>
-                <br />
-                {showGroups ? (
-                    Object.keys(groups).map((group, i) => {
-
-                        return (
-                            <div key={group} className="m-2 text-center flex-wrap w-25 border shadow shadow-sm rounded">
-                                <ol>
-                                    <strong>{group}</strong>
-                                    {groups[group].map(student => {
-                                        return (
-                                            <li key={student.student}>{student.student}</li>
-                                        )
-                                    })}
-                                </ol>
-                            </div>
-                        )
-                    }
-
-                    )
-                ) : (
-                    <div className="d-flex align-items-center justify-content-center text-center my-5 p-5 w-100">
-                        <strong>{gradeData ? 'Ready to form groups!' : 'Waiting on data...'}</strong>
-                    </div>
-                )
-                }
-            </div>
-            {showGroups ? (
-                < button className={`btn btn-lg ${data ? 'btn-secondary' : error ? 'btn-danger' : 'btn-secondary'}`} onClick={handleSaveClick} disabled={disableSave}>{data ? 'Groups Saved!' : error ? 'There was a problem!' : 'Save Groups!'}</button>
-
+      {gradeData ? (
+        <div className="d-flex justify-content-center w-100 p-2 mt-2 mb-1 w-25 bg-dark text-light">
+          <form
+            onSubmit={handleGroupButtonClick}
+            className="d-flex flex-column justify-content-center"
+          >
+            <label className="form-label" htmlFor="numGroups">
+              <strong>Desired number of groups:</strong>
+            </label>
+            <input
+              className="form-control mb-2"
+              type="number"
+              min="1"
+              name="numGroups"
+              id="numGroups"
+              value={numGroups}
+              onChange={handleGroupNumChange}
+            />
+            {cohortGroups.length > 0 ? (
+              <div className="w-100 text-center px-5">
+                <label className="form-check-label" htmlFor="memberRepeat">
+                  <strong>
+                    Place students who have worked together in separate groups
+                    (no repeating members):
+                  </strong>
+                </label>
+                <input
+                  className="form--check-input"
+                  type="checkbox"
+                  checked={memberRepeat}
+                  onChange={() => setMemberRepeat(!memberRepeat)}
+                />
+              </div>
             ) : null}
-        </div >
-    )
+
+            <div
+              className="button-wrapper w-100 d-flex justify-content-center p-1"
+              style={{ height: "3rem" }}
+            >
+              {showButton ? (
+                <button
+                  className="btn btn-secondary"
+                  type="submit"
+                  onClick={handleGroupButtonClick}
+                >
+                  Form Groups
+                </button>
+              ) : null}
+            </div>
+          </form>
+        </div>
+      ) : (
+        <div className="d-flex align-items-center my-5 p-5">
+          <strong>Preparing student data...</strong>
+          <div
+            className="spinner-border ms-auto"
+            role="status"
+            aria-hidden="true"
+          ></div>
+        </div>
+      )}
+      <div
+        className="groups-wrapper d-flex justify-content-center flex-wrap"
+        style={{ border: "1px solid black" }}
+      >
+        <br />
+        {showGroups ? (
+          Object.keys(groups).map((group, i) => {
+            return (
+              <div
+                key={group}
+                className="m-2 text-center flex-wrap w-25 border shadow shadow-sm rounded"
+              >
+                <ol>
+                  <strong>{group}</strong>
+                  {groups[group].map((student) => {
+                    return <li key={student.student}>{student.student}</li>;
+                  })}
+                </ol>
+              </div>
+            );
+          })
+        ) : (
+          <div className="d-flex align-items-center justify-content-center text-center my-5 p-5 w-100">
+            <strong>
+              {gradeData ? "Ready to form groups!" : "Waiting on data..."}
+            </strong>
+          </div>
+        )}
+      </div>
+      {showGroups ? (
+        <button
+          className={`btn btn-lg ${error ? "btn-danger" : "btn-secondary"}`}
+          onClick={handleSaveClick}
+          disabled={disableSave}
+        >
+          {data
+            ? "Groups Saved!"
+            : error
+            ? "There was a problem!"
+            : "Save Groups!"}
+        </button>
+      ) : null}
+    </div>
+  );
 }
--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../../utils/mutations';
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../utils/mutations";
 
-import logo2u from '../../assets/images/2uLogo.png'
+import logo2u from "../../assets/images/2uLogo.png";
 
-import Auth from '../../utils/auth';
-import './style.css';
+import Auth from "../../utils/auth";
+import "./style.css";
 
 const Signup = () => {
   const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    password: '',
-    bcsEmail: '',
-    bcsPassword: ''
-
+    name: "",
+    email: "",
+    password: "",
+    bcsEmail: "",
+    bcsPassword: "",
   });
   const [sameInfoCheckbox, setSameInfoCheckbox] = useState(false);
   const [addUser, { error, data }] = useMutation(ADD_USER);
-  if (error) { console.log(JSON.stringify(error)) }
+  if (error) {
+    console.log(JSON.stringify(error));
+  }
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -44,15 +45,14 @@ const Signup = () => {
   // }
 
   const handleSameInfoClick = (e) => {
-    setSameInfoCheckbox(!sameInfoCheckbox)
+    setSameInfoCheckbox(!sameInfoCheckbox);
     // if the user clicks the box, set bcslogin state to matching login values
     setFormState({
       ...formState,
-      bcsEmail: !sameInfoCheckbox ? formState.email : '',
-      bcsPassword: !sameInfoCheckbox ? formState.password : ''
-
-    })
-  }
+      bcsEmail: !sameInfoCheckbox ? formState.email : "",
+      bcsPassword: !sameInfoCheckbox ? formState.password : "",
+    });
+  };
 
   // submit form
   const handleFormSubmit = async (e) => {
@@ -64,11 +64,11 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState },
       });
-      console.log("create user post-data: ", data.addUser.token)
+      console.log("create user post-data: ", data.addUser.token);
 
       Auth.login(data.addUser.token);
     } catch (err) {
-      console.log(JSON.stringify(error))
+      console.log(JSON.stringify(error));
       console.error(err);
     }
   };
@@ -76,9 +76,15 @@ const Signup = () => {
   return (
     <>
       <div className="row main-signup-content bg-success text-center">
-
         <div className="col-md-2 text-center company__info">
-          <div className="logo__wrapper p-2 d-flex justify-content-center align-items-center" style={{ background: 'white', borderRadius: '50%', aspectRatio: '1/1' }}>
+          <div
+            className="logo__wrapper p-2 d-flex justify-content-center align-items-center"
+            style={{
+              background: "white",
+              borderRadius: "50%",
+              aspectRatio: "1/1",
+            }}
+          >
             <img className="company__logo" src={logo2u} alt="2U Logo" />
           </div>
           {/* <h4 className="company_title">BCS Data Viewer</h4> */}
@@ -86,17 +92,20 @@ const Signup = () => {
 
         <div className="col-md-10 col-xs-12 col-sm-12 login_form ">
           <div className="container-fluid">
-
             <div className="row">
               <h2>Sign Up!</h2>
             </div>
 
             {/* <div className="row"> */}
-            <form control="" className="form-group row" onSubmit={handleFormSubmit}>
+            <form
+              control=""
+              className="form-group row"
+              onSubmit={handleFormSubmit}
+            >
               <div className="col-lg-6">
                 <div className="row">
-
                   <input
+                    required
                     className="form-input form__input"
                     placeholder="Your username"
                     autoComplete="username"
@@ -110,6 +119,7 @@ const Signup = () => {
 
                 <div className="row">
                   <input
+                    required
                     className="form-input form__input"
                     placeholder="Your email"
                     aria-label="email"
@@ -123,6 +133,7 @@ const Signup = () => {
                 <div className="row">
                   {/* <!-- <span className="fa fa-lock"></span> --> */}
                   <input
+                    required
                     className="form-input form__input"
                     placeholder="******"
                     autoComplete="new-password"
@@ -134,15 +145,19 @@ const Signup = () => {
                   />
                 </div>
               </div>
-              <div className="col-lg-6 p-3 signup-bcs-info text-light">
+              <div className="col-lg-6 p-3 signup-bcs-info text-light rounded shadow">
                 <div className="row">
                   <h3>BCS Information</h3>
                 </div>
                 <div className="row">
-                  <label htmlFor="sameInfoCheckbox">
-                    Use same Login info:
+                  <label
+                    className="form-check-label"
+                    htmlFor="sameInfoCheckbox d-inline"
+                  >
+                    Use same info:
                   </label>
                   <input
+                    className="d-inline form-check"
                     name="sameInfoCheckbox"
                     id="sameInfoCheckbox"
                     type="checkbox"
@@ -150,40 +165,45 @@ const Signup = () => {
                     checked={sameInfoCheckbox}
                     onChange={handleSameInfoClick}
                   />
-
                 </div>
                 <div className="row">
                   <input
+                    required
                     className="form-input form__input"
                     placeholder="BCS Login Email"
                     aria-label="bootcamp spot email"
                     name="bcsEmail"
                     type="email"
                     readOnly={sameInfoCheckbox}
-                    value={!sameInfoCheckbox ? formState.bcsEmail : formState.email}
+                    value={
+                      !sameInfoCheckbox ? formState.bcsEmail : formState.email
+                    }
                     onChange={handleChange}
                   />
                 </div>
                 <div className="row">
                   <input
+                    required
                     className="form-input form__input"
                     placeholder="BCS Login Password"
                     aria-label="bootcamp spot password"
                     name="bcsPassword"
                     type="password"
                     readOnly={sameInfoCheckbox}
-                    value={!sameInfoCheckbox ? formState.bcsPassword : formState.password}
+                    value={
+                      !sameInfoCheckbox
+                        ? formState.bcsPassword
+                        : formState.password
+                    }
                     onChange={handleChange}
                   />
                 </div>
-
               </div>
-
 
               <div className="row">
                 <button
                   className="form-btn w-100"
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: "pointer" }}
                   type="submit"
                 >
                   Submit
@@ -193,16 +213,15 @@ const Signup = () => {
             {/* </div> */}
 
             <div className="row">
-              <p>Already have an account?  <Link to="/login">Login Here</Link></p>
+              <p>
+                Already have an account? <Link to="/login">Login Here</Link>
+              </p>
             </div>
-
           </div>
         </div>
       </div>
     </>
-
   );
 };
 
 export default Signup;
-
